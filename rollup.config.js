b3b638cfd00c7445aa8b6811d92301ae3502a119
abschill <ajay198810@gmail.com>
2022-07-06 13:37:14 -0600

    chore: add #1

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+// Import rollup plugins
+import html from '@web/rollup-plugin-html';
+import {copy} from '@web/rollup-plugin-copy';
+import resolve from '@rollup/plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
+import summary from 'rollup-plugin-summary';
+
+export default {
+	input: './build/index.js',
+	plugins: [
+		// Resolve bare module specifiers to relative paths
+		resolve(),
+		// Minify HTML template literals
+		minifyHTML(),
+		// Minify JS
+		terser({
+			ecma: 2020,
+			module: true,
+			warnings: true,
+		}),
+	],
+	output: {
+		dir: 'bundle',
+	},
+	preserveEntrySignatures: 'strict',
+};
