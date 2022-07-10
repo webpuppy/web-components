584be8dfe610ce4c8ffc9c963c0d756912052afe
abschill <ajay198810@gmail.com>
2022-07-10 09:28:59 -0600

    chore: close #20

--- src/components/link/index.ts
+++ src/components/link/index.ts
@@ -8,14 +8,22 @@ import {
     when,
 } from '../../globals';
 import { LINK_STYLES, CSS_RESETS } from './link.css';
+import { LinkTarget } from './defs';
 
 @customElement(Enums.default.COMPONENT_PREFIX + 'link')
 export class WPLink extends LitElement {
-    @property({ attribute: true, type: String }) href: string = '#';
-    @property({ attribute: true, type: String }) text: string = '';
+
+	@property({ attribute: true, type: String })
+	target: LinkTarget = '_self';
+
+    @property({ attribute: true, type: String })
+	href: string = '#';
+
+    @property({ attribute: true, type: String })
+	text: string = '';
 
     static override styles = [CSS_RESETS, LINK_STYLES];
     render() {
-        return html` <a href="${this.href}">${this.text}</a> `;
+        return html` <a href="${this.href}" target=${this.target}>${this.text}</a> `;
     }
 }
