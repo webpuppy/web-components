e0af27b0bde04f7a6aefcd123007b03720ba1695
abschill <ajay198810@gmail.com>
2022-07-10 18:18:26 -0600

    fix: type class converter for layout

--- src/components/container/layout/index.ts
+++ src/components/container/layout/index.ts
@@ -5,9 +5,7 @@ import {
     LitElement,
     Enums,
     html,
-    property,
-    classMap,
-    when,
+    property
 } from '../../../globals/exports';
 
 @customElement(Enums.default.COMPONENT_PREFIX + 'layout')
@@ -20,20 +18,16 @@ export class WPLayout extends LitElement {
 
     private _convertTypeToClass() {
         switch (this.type) {
-            case 'topbar-0':
-                return 'wp-layout-tbr-0';
             case 'topbar-1':
                 return 'wp-layout-tbr-1';
+			case 'sidebar-0':
+				return 'wp-layout-sbr-0';
             case 'sidebar-1':
                 return 'wp-layout-sbr-1';
             default:
-                return 'wp-layout-sbr-0';
+                return 'wp-layout-tbr-0';
         }
     }
-    /**
-     *
-     * todo: setup dynamic grid sizing on children
-     */
 
     render() {
         return html`
