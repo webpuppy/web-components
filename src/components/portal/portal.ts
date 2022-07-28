f1fae61a7b74bd275a4105cab82b39c9ba9fc7aa
abschill <ajay198810@gmail.com>
2022-07-28 11:00:10 -0600

    :sparkles: close #45

--- src/components/portal/portal.ts
+++ src/components/portal/portal.ts
@@ -1,18 +1,37 @@
 import {
     Enums,
-	CSS_RESETS
+	WPSizeable
 } from '../../globals/exports';
 import {
 	customElement,
-    LitElement,
     html,
     property,
 } from 'lit-exports';
 
 @customElement(Enums.default.COMPONENT_PREFIX + 'portal')
-export class WPPortal extends LitElement {
+export class WPPortal extends WPSizeable {
+
+	@property()
+	src: string;
 
 	render() {
-		return html``;
+		if(this.size === 'xs') {
+			return html`
+			<iframe width="180" src=${this.src}></iframe>
+		`;
+		}
+		if(this.size === 'sm') {
+			return html`
+			<iframe width="360" src=${this.src}></iframe>
+		`;
+		}
+		if(this.size === 'xl') {
+			return html`
+			<iframe width="1920" src=${this.src}></iframe>
+		`;
+		}
+		return html`
+			<iframe width="720" src=${this.src}></iframe>
+		`;
 	}
 }
