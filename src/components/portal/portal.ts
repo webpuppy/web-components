932a7589c41e405f14ed204761478b92ecbf77ab
abschill <ajay198810@gmail.com>
2022-07-28 14:08:03 -0600

    :art: :sparkles: portal styles@0.0.29

--- src/components/portal/portal.ts
+++ src/components/portal/portal.ts
@@ -1,12 +1,14 @@
 import {
     Enums,
-	WPSizeable
+	WPSizeable,
+	CSS_RESETS
 } from '../../globals/exports';
 import {
 	customElement,
     html,
     property,
 } from 'lit-exports';
+import portalCSS from './style.css';
 
 @customElement(Enums.default.COMPONENT_PREFIX + 'portal')
 export class WPPortal extends WPSizeable {
@@ -14,24 +16,26 @@ export class WPPortal extends WPSizeable {
 	@property()
 	src: string;
 
+	static override styles = [CSS_RESETS, portalCSS];
+
 	render() {
 		if(this.size === 'xs') {
 			return html`
-			<iframe width="180" src=${this.src}></iframe>
+			<iframe width="180" height="360" src=${this.src}></iframe>
 		`;
 		}
 		if(this.size === 'sm') {
 			return html`
-			<iframe width="360" src=${this.src}></iframe>
+			<iframe width="360" height="360" src=${this.src}></iframe>
 		`;
 		}
 		if(this.size === 'xl') {
 			return html`
-			<iframe width="1920" src=${this.src}></iframe>
+			<iframe width="1920" height="1080" src=${this.src}></iframe>
 		`;
 		}
 		return html`
-			<iframe width="720" src=${this.src}></iframe>
+			<iframe width="720" height="540" src=${this.src}></iframe>
 		`;
 	}
 }
