4101f01c620097622128a4ea3d5b45ccf2fc7086
abschill <ajay198810@gmail.com>
2022-07-31 08:30:28 -0600

    :sparkles: close #51

--- src/components/fade-up/fade-up.css.ts
+++ src/components/fade-up/fade-up.css.ts
@@ -3,6 +3,7 @@ import { css } from '../../globals/exports';
 export default css`
 :host {
 	animation: fade-up 350ms ease-in-out;
+	display: inline-block;
 	opacity: 1;
 	position: relative;
 	transform: translateY(0);
@@ -11,7 +12,7 @@ export default css`
 @keyframes fade-up {
 	0% {
 		opacity: 0;
-		transform: translateY(2rem);
+		transform: translateY(1rem);
 	}
 }
 `;
