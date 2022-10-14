a78bf6d98347d64c4ee81db60d7cac0085594d96
abschill <ajay198810@gmail.com>
2022-10-14 15:46:21 -0600

    :wrench: update lib build script & close #92

--- scripts/rollup.bundle.mjs
+++ scripts/rollup.bundle.mjs
@@ -2,27 +2,14 @@ import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
+import babelConfig from '../babel.config.cjs';
 
-export const assumptions = {
-	'setPublicClassFields': true
-};
-
-export const plugins = [
-	['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true } ],
-	['@babel/plugin-proposal-class-properties'],
-];
-
-export const presets = [
-	'@babel/preset-env',
-	'@babel/preset-react',
-	'@babel/preset-typescript'
-]
 export default {
 	input: './src/index.ts',
 	plugins: [
 		typescript(),
 		resolve(),
-		babel({ assumptions, plugins, presets }),
+		babel(babelConfig),
 		// Minify JS
 		terser({
 			ecma: 2015,
--- scripts/rollup.lib.mjs
+++ scripts/rollup.lib.mjs
@@ -1,22 +1,22 @@
 import resolve from '@rollup/plugin-node-resolve';
-import styles from 'rollup-plugin-styles';
-import dts from 'rollup-plugin-dts';
 import babel from '@rollup/plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 import peers from 'rollup-plugin-peer-deps-external';
-import pkg from '../package.json' assert { type: 'json' };
 import babelConfig from '../babel.config.cjs';
 import tsConfig from '../tsconfig.json' assert { type: 'json' };
+import glob from 'glob';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-/**
- * todo - split output modules into single files per, 
- * to assist with code splitting
- */
 export default [
 	{
-		input: './src/index.ts',
+		input: Object.fromEntries(
+			glob.sync('src/**/*.ts').map(file => [
+				path.relative('src', file.slice(0, file.length - path.extname(file).length)),
+				fileURLToPath(new URL(`../${file}`, import.meta.url))
+			])
+		),
 		plugins: [
-			styles(),
 			peers(),
 			resolve(),
 			typescript({ ...tsConfig.compilerOptions, declaration: true, declarationDir: 'lib' }),
@@ -24,25 +24,10 @@ export default [
 		],
 		output: [
 			{
-				file: pkg.main,
-				format: 'cjs',
-				sourcemap: true,
-			},
-			{
-				file: pkg.module,
+				dir: 'lib',
 				format: 'esm',
 				sourcemap: true,
 			}
   	],
-	},
-	{
-		input: 'lib/esm/index.d.ts',
-		output: { 
-			file: 'lib/index.d.ts',
-			format: 'esm' 
-		},
-		plugins: [
-			dts()
-		],
 	}
 ];
