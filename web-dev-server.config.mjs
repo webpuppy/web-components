4fa1610c79391b9b762d1a46c40c43b3ae767e6c
abschill <ajay198810@gmail.com>
2022-10-14 08:51:01 -0600

    wds test runner setup

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -8,21 +8,30 @@ import { legacyPlugin } from '@web/dev-server-legacy';
 
 const mode = process.env.MODE || 'dev';
 if (!['dev', 'prod'].includes(mode)) {
-  throw new Error(`MODE must be "dev" or "prod", was "${mode}"`);
+	throw new Error(`MODE must be "dev" or "prod", was "${mode}"`);
 }
 
 export default {
-  nodeResolve: {exportConditions: mode === 'dev' ? ['development'] : []},
-  preserveSymlinks: true,
-  port: 3000,
-  plugins: [
-    legacyPlugin({
-		polyfills: {
-			// Manually imported in index.html file
-			webcomponents: true,
-			coreJs: true,
-        	fetch: true,
-		},
-    }),
-  ],
+	nodeResolve: {exportConditions: mode === 'dev' ? ['development'] : []},
+	preserveSymlinks: true,
+	port: 3000,
+	plugins: [
+		legacyPlugin({
+			polyfills: {
+				// Manually imported in index.html file
+				webcomponents: true,
+				coreJs: true,
+				fetch: true,
+			},
+			custom: [
+				{
+					name: 'lit-polyfill-support',
+					path: 'node_modules/lit/polyfill-support.js',
+					// eslint-disable-next-line quotes
+					test: "!('attachShadow' in Element.prototype)",
+					module: false,
+				},
+			],
+		}),
+	],
 };
